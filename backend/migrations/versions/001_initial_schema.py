bc9dcc3850a517b01240172ee080bf61faa2f244
mark-ny <markomodho@gmail.com>
2026-04-04 12:30:13 +0300

    fix: add database migrations folder

---
+++ backend/migrations/versions/001_initial_schema.py
@@ -0,0 +1,23 @@
+"""initial schema
+
+Revision ID: 001
+Revises: 
+Create Date: 2026-01-01 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '001'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
